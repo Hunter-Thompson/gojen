10fedbc944601b7c2d0c6bd1cb6aa8a803697fc5
Hunter Thompson <aatman.bos@smallcase.com>
2021-11-21 06:24:28 +0530

    feat(release): upload binary

--- go.mod
+++ go.mod
@@ -2,14 +2,9 @@ module github.com/Hunter-Thompson/gojen
 
 go 1.17
 
-require (
-	github.com/spf13/cobra v1.2.1
-	gopkg.in/yaml.v2 v2.4.0
-)
+require github.com/spf13/cobra v1.2.1
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/kr/pretty v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- pkg/project/project.go
+++ pkg/project/project.go
@@ -9,8 +9,6 @@ import (
 	"os/exec"
 	"reflect"
 	"strings"
-
-	"gopkg.in/yaml.v2"
 )
 
 type IProject interface {
@@ -34,6 +32,10 @@ type IProject interface {
 	IsGoLinter() bool
 	IsGoTest() bool
 	GetGoTestArgs() []string
+	IsBuildWorkflow() bool
+	GetGitHubToken() string
+	GetGojenVersion() string
+	IsIsGojen() bool
 }
 
 type Project struct {
@@ -46,12 +48,14 @@ type Project struct {
 	AuthorEmail        *string `yaml:"authorEmail" json:"authorEmail"`
 	AuthorOrganization *string `yaml:"authorOrganization" json:"authorOrganization"`
 
-	Licensed *bool `yaml:"licensed" json:"licensed"`
+	Licensed     *bool   `yaml:"licensed" json:"licensed"`
+	GojenVersion *string `yaml:"gojenVersion" json:"gojenVersion"`
 
 	Release              *bool   `yaml:"release" json:"release"`
 	BuildWorkflow        *bool   `yaml:"buildWorkflow" json:"buildWorkflow"`
 	GithubToken          *string `yaml:"githubToken" json:"githubToken"`
 	DefaultReleaseBranch *string `yaml:"defaultReleaseBranch" json:"defaultReleaseBranch"`
+	IsGojen              *bool   `yaml:"isGojen" json:"isGojen"`
 
 	Gitignore  *[]string `yaml:"gitignore" json:"gitignore"`
 	CodeOwners *[]string `yaml:"codeOwners" json:"codeOwners"`
@@ -104,7 +108,7 @@ func GetConfig() (*Project, error) {
 		return nil, err
 	}
 
-	err = yaml.Unmarshal(b, &proj)
+	err = json.Unmarshal(b, &proj)
 	if err != nil {
 		return nil, err
 	}
@@ -336,6 +340,20 @@ func (proj *Project) CreateReleaseWorkflow() error {
 		return err
 	}
 
+	var gojenCommand string
+
+	if proj.IsIsGojen() {
+		gojenCommand = `- name: build and run gojen
+	    run: "go build && gojen"
+		`
+	} else {
+		gojenCommand = `- name: Install gojen
+      run: go install github.com/Hunter-Thompson/gojen
+	  - name: Run gojen
+	    run: gojen 
+		`
+	}
+
 	c := fmt.Sprintf(`name: Release
 on:
   push:
@@ -352,10 +370,7 @@ jobs:
       uses: actions/setup-go@v2
       with:
         go-version: %s
-	  - name: Install gojen
-      run: go install github.com/Hunter-Thompson/gojen
-	  - name: Run gojen
-	    run: gojen 
+		%s
   release:
     needs:
     - build
@@ -367,9 +382,33 @@ jobs:
         with:
           github-token: ${{ secrets.%s }}
           changelog-generator-opt: "emojis=false"
-          force-bump-patch-version: true`, proj.GetDefaultReleaseBranch(), proj.GetGoVersion(), proj.GetGitHubToken())
+          force-bump-patch-version: true`, proj.GetDefaultReleaseBranch(), proj.GetGoVersion(), gojenCommand, proj.GetGitHubToken())
 
 	err = ioutil.WriteFile(fmt.Sprintf("%s/.github/workflows/release.yml", pwd), []byte(c), 0644)
+	if err != nil {
+		return err
+	}
+
+	c = fmt.Sprintf(`name: upload binary
+
+on:
+  release:
+    types: [published]
+
+jobs:
+  release:
+    name: release 
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - uses: wangyoucao577/go-release-action@v1.19
+      with:
+        github_token: ${{ secrets.%s }}
+        goos: linux
+        goarch: amd64
+        goversion: %s`, proj.GetGitHubToken(), proj.GetGoVersion())
+
+	err = ioutil.WriteFile(fmt.Sprintf("%s/.github/workflows/upload-binary.yml", pwd), []byte(c), 0644)
 	if err != nil {
 		return err
 	}
@@ -391,24 +430,56 @@ func (proj *Project) CreateBuildWorkflow() error {
 		return err
 	}
 
+	var gojenCommand string
+
+	if proj.IsIsGojen() {
+		gojenCommand = `- name: build and run gojen
+	    run: "go build && gojen"`
+	} else {
+		gojenCommand = fmt.Sprintf(`- name: Install gojen
+      run: go install github.com/Hunter-Thompson/gojen@%s
+	  - name: Run gojen
+	    run: gojen `, proj.GetGojenVersion())
+	}
+
 	c := fmt.Sprintf(`name: Build
 on:
   pull_request: {}
 
 jobs:
   build:
-	runs-on: ubuntu:latest
-	name: Build
-	steps:
-	- uses: actions/checkout@v2
-	- name: Setup go 
-	  uses: actions/setup-go@v2
-	  with:
-		go-version: %s
-	  - name: Install gojen
-	  run: go install github.com/Hunter-Thompson/gojen
-	  - name: Run gojen
-	    run: gojen`, proj.GetGoVersion())
+	  runs-on: ubuntu:latest
+	  name: Build
+	  steps:
+	  - uses: actions/checkout@v2
+        with:
+          ref: ${{ github.event.pull_request.head.ref }}
+          repository: ${{ github.event.pull_request.head.repo.full_name }}
+	  - name: Setup go 
+	    uses: actions/setup-go@v2
+	    with:
+	  	go-version: %s
+	  %s
+    - name: Check for changes
+      id: git_diff
+      run: git diff --exit-code || echo "::set-output name=has_changes::true"
+    - if: steps.git_diff.outputs.has_changes
+      name: Commit and push changes (if changed)
+      run: 'git add . && git commit -m "chore: self mutation" && git push origin
+        HEAD:${{ github.event.pull_request.head.ref }}'
+    - if: steps.git_diff.outputs.has_changes
+      name: Update status check (if changed)
+      run: gh api -X POST /repos/${{ github.event.pull_request.head.repo.full_name
+        }}/check-runs -F name="build" -F head_sha="$(git rev-parse HEAD)" -F
+        status="completed" -F conclusion="success"
+      env:
+        GITHUB_TOKEN: ${{ secrets.%s }}
+    - if: steps.git_diff.outputs.has_changes
+      name: Cancel workflow (if changed)
+      run: gh api -X POST /repos/${{ github.event.pull_request.head.repo.full_name
+        }}/actions/runs/${{ github.run_id }}/cancel
+      env:
+        GITHUB_TOKEN: ${{ secrets.%s }}`, proj.GetGoVersion(), gojenCommand, proj.GetGitHubToken(), proj.GetGitHubToken())
 
 	err = ioutil.WriteFile(fmt.Sprintf("%s/.github/workflows/build.yml", pwd), []byte(c), 0644)
 	if err != nil {
@@ -535,6 +606,21 @@ func (proj *Project) IsBuildWorkflow() bool {
 		return false
 	}
 	return *proj.BuildWorkflow
+}
+
+func (proj *Project) IsIsGojen() bool {
+	if proj.IsGojen == nil {
+		return false
+	}
+
+	return *proj.IsGojen
+}
+
+func (proj *Project) GetGojenVersion() string {
+	if proj.GojenVersion == nil {
+		return "0.1.0"
+	}
+	return *proj.GojenVersion
 }
 
 func String(str string) *string {
